7fe27e0d5545951e23741a4492d8cc56c0c73cca
Fascinate <eddo92@yahoo.it>
2024-11-23 19:18:15 +0100

    Update menu-con-ciclo-do.cs

--- menu-con-ciclo-do.cs
+++ menu-con-ciclo-do.cs
@@ -8,17 +8,21 @@ do
     int scelta = int.Parse(Console.ReadLine());
     if (scelta == 1)
     {
-        stato = 1;
-        Console.WriteLine("hai scelto di andare avanti\nPremere 1 per andare ancora avanti o 2 per tornare indietro");
-        scelta = int.Parse(Console.ReadLine());
+        Console.WriteLine("hai scelto di andare avanti");
+        stato1(ref stato, ref scelta);
         if (scelta == 1)
         {
-            Console.WriteLine("sei andato avanti\nPremere 2 per al menu iniziale");
+            Console.WriteLine("sei andato avanti\nPremere 1 per al menu iniziale o 2 per andare indietro");
             scelta = int.Parse(Console.ReadLine());
-            if (scelta == 2)
+            if (scelta == 1)
             {
                 stato = 0;
                 Console.Clear();
+            }
+            else if ( scelta == 2)
+            {
+                Console.WriteLine("sei tornato indietro di un menu");
+                stato1(ref stato, ref scelta);
             }
         }
         else if (scelta == 2)
@@ -51,4 +55,12 @@ while (stato==0);
 static void introduzione(){
 
     Console.WriteLine("Premere\n1.Per andare avanti\n2.Per uscire");
+}
+
+static void stato1(ref int stato,ref int scelta)
+{
+    stato = 1;
+    Console.WriteLine("Premere 1 per andare ancora avanti o 2 per tornare indietro");
+    scelta = int.Parse(Console.ReadLine());
+    
 }
